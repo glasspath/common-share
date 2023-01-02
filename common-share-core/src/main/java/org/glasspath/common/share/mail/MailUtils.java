52988946913246ddae758e9daca4d8e44f6cf1c6
remcopoelstra <remco@poelstrabesturingen.nl>
2023-01-02 17:52:56 +0100

    Improved parsing of recipients

--- common-share-core/src/main/java/org/glasspath/common/share/mail/MailUtils.java
+++ common-share-core/src/main/java/org/glasspath/common/share/mail/MailUtils.java
@@ -41,6 +41,8 @@ public class MailUtils {
 
 		if (recipients != null) {
 
+			recipients = recipients.toLowerCase();
+
 			// https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
 			Matcher matcher = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])").matcher(recipients); //$NON-NLS-1$
 			while (matcher.find()) {
@@ -108,7 +110,7 @@ public class MailUtils {
 		return s;
 
 	}
-	
+
 	public static String createElementsString(List<String> elements, String prepend, String append, String separator) {
 
 		String s = ""; //$NON-NLS-1$
@@ -166,7 +168,7 @@ public class MailUtils {
 		}
 
 	}
-	
+
 	public static URI createGmailMailtoUri(Mailable mailable) throws ShareException {
 
 		try {
